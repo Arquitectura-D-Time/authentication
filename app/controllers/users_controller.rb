3b01ee0000feae7d9dcf44b2904c0c6a77ecb623
Rafael Cruz <ercruzr@unal.edu.co>
2019-10-08 13:07:28 -0500

    registro y login de usuario

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  
+  before_action :set_user, only: [:show]
+
+  def show 
+	  @user = User.find(params[:id])
+	  render json: @user
+  end 
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+  
+    def user_params
+      params.require(:user).permit(:id)
+    end
+end
