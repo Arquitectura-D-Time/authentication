7660d2b94b4d98d473d7352c2e9de2b7583f941c
Davsatu313 <dsfajardob@unal.edu.co>
2019-11-17 20:22:12 -0500

    ldap added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :users, only: [:show, :create, :index]
+  resources :ldap
+  resources :users, only: [:show]
   mount_devise_token_auth_for 'User', at: 'auth',
-  controllers: {
-    sessions: 'sessions'
-  }
+    controllers: {
+      sessions: 'sessions'
+    }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
