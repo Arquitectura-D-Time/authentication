030872ed6b5c8ec1a8eadb4626d547e533137900
Rafael Cruz <ercruzr@unal.edu.co>
2019-11-19 12:41:37 -0500

    users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show, :index]
 
   def show 
 	  @user = User.find(params[:id])
