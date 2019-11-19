3488b8dc33ec87261d9fe7ebcd04c5fbba7a1650
Rafael Cruz <ercruzr@unal.edu.co>
2019-11-19 13:46:31 -0500

    error users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   
-  before_action :set_user, only: [:show, :index]
+  before_action :set_user, only: [:show]
 
   def show 
 	  @user = User.find(params[:id])
