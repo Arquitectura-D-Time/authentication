1eae20dcad99c36f0b0d8b1658d72e5fb0bb89d0
Rafael Cruz <ercruzr@unal.edu.co>
2019-12-17 18:15:54 -0500

    error rancher

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,9 @@
-bundle exec rails db:create && bundle exec rails db:migrate && bundle exec rails db:setup && rm -f /authentication_ms/tmp/pids/server.pid && bundle exec rails s -p 5001 -b 0.0.0.0
+#!/bin/sh
+
+bundle exec rails db:create
+bundle exec rails db:migrate
+bundle exec rails db:setup
+
+rm -f /authentication_ms/tmp/pids/server.pid
+
+bundle exec rails s -p 5001 -b 0.0.0.0
